5e890ebe4a87d9c86f97df2d542b7c0980fc4e4e
FHD1920x1080 <terwq135@gmail.com>
2023-05-26 13:34:02 +0900

    웹오픈 함수 추가

--- term/utils.py
+++ term/utils.py
@@ -1,5 +1,6 @@
 #잡스러운 함수들 모아두는 파일
 import ctypes
+import webbrowser
 def get_windows_text_scaling():
     user32 = ctypes.windll.user32
     # DPI 스케일링 모드 가져오기
@@ -7,3 +8,6 @@ def get_windows_text_scaling():
     # 확대 비율 계산
     text_scaling = dpi_scaling / 96.0
     return text_scaling
+
+def webOpen(url):
+    webbrowser.open(url)
