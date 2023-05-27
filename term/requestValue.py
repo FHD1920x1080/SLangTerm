a49c04d748fb1670e7e0f40b00c14906822f7876
FHD1920x1080 <terwq135@gmail.com>
2023-05-27 09:43:47 +0900

    tkWindow init에서 프레임 초기화 하는거 함수로 돌려놓음
    setParams에서 Params가 바뀌었는지 검사하는거 없애고 출력버튼에서 그냥 무조건 1페이지 검색하게 바꿈

--- term/requestValue.py
+++ term/requestValue.py
@@ -11,45 +11,26 @@ class RequestValue:
         self.bgnde = StringVar()
         self.endde = StringVar()
         self.upkind = StringVar()
-        self.upkind.set('0') # 라디오박스에 쓸거면 빈무자열 안됨.
+        self.upkind.set(' ') # 라디오박스에 쓸거면 ''같은 빈문자열은 안되고 쓰레기 값이라도 있어야함 그래서 공백 한칸 씀
         self.state = StringVar()
-        self.state.set('0')
+        self.state.set(' ')
 
 
     def setParams(self, key, value):
-        if key in self.queryParams: # 키가 있는지
-            if value:  # 값이 있으면
-                if self.queryParams[key] != value:# 이전이랑 다른값인지 같으면 수정 안함
-                    self.queryParams[key] = value
-                    self.pageNo = 1 #다르면 페이지 초기화
-            else: # 없으면 삭제하고 페이지 초기화
-                del self.queryParams[key]
-                self.pageNo = 1
-        else:
-            if value:
-                self.queryParams[key] = value
-                self.pageNo = 1  # 다르면 페이지 초기화
-    def setParamsRadio(self, key, value):
-        if key in self.queryParams: # 키가 있는지
-            if value != '0':  # 값이 있으면
-                if self.queryParams[key] != value:# 이전이랑 다른값인지 같으면 수정 안함
-                    self.queryParams[key] = value
-                    self.pageNo = 1 #다르면 페이지 초기화
-            else: # 없으면 삭제하고 페이지 초기화
-                del self.queryParams[key]
-                self.pageNo = 1
-        else:
-            if value != '0':  # 값이 있으면
-                self.queryParams[key] = value
-                self.pageNo = 1  # 다르면 페이지 초기화
+        if value and value != ' ': # 값이 있으면, 공백 제거로 할 수도 있지만 이게 더 빠를듯
+            self.queryParams[key] = value
+        else: #
+            self.queryParams.pop(key, None) # 없으면 None이 반환되는데 딱히 반환값은 필요 없음
 
     def set(self):
+        tempParams = self.queryParams
         self.setParams('bgnde', self.bgnde.get())
         self.setParams('endde', self.endde.get())
-        self.setParamsRadio('upkind', self.upkind.get())
-        self.setParamsRadio('state', self.state.get())
+        self.setParams('upkind', self.upkind.get())
+        self.setParams('state', self.state.get())
         # 지역코드 set 추가해야함
-        self.queryParams['numOfRows'] = str(self.numOfRows)
-        self.queryParams['pageNo'] = str(self.pageNo)
+
+        self.setParams('numOfRows', str(self.numOfRows))
+        self.setParams('pageNo', str(self.pageNo))
     def getValue(self):
         return requests.get(url, params=self.queryParams)
